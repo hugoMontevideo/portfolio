41311dfccbaa9ba6de10e833df826d0143f47f5a
hugoMontevideo <machadopereira.hugo@gmail.com>
2023-05-06 00:01:29 +0200

    premier commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+// const burgerButton = document.querySelector(".mobile");
+const hamburgerButton = document.querySelector(".nav-toggler");
+
+const navCurtain = document.querySelector(".nav-curtain");
+
+function toggleNone(){
+    navCurtain.classList.toggle("active");
+    hamburgerButton.classList.toggle("active");
+}
+
+hamburgerButton.addEventListener("click", toggleNone);
